d60dfa1da47b9ceb1d05819e8441e2fbafb530fd
marifw70 <70124547+marifw70@users.noreply.github.com>
2020-08-27 13:35:26 +0700

    Update urai.py

--- urai.py
+++ urai.py
@@ -1,10 +1,10 @@
 nama = 'Code' # set nama yang mau diuji
 def urai(kata): # set function dengan nama urai dan parameternya adalah kata
     z=[]
-    for row in range(len(nama)): #looping row pada jumlah kata 
-        for column in range(0,row): # lopping column sesuai dengan jumlah row 
-            z+=list(nama[:row]) # tambah value list dengan index yang panjangya sesuai hasil looping row 
-        z+=list(nama[row]) #tambah value list denga index yang panjangnya sesuai dengan row
+    for row in range(len(kata)): #looping row pada jumlah kata 
+        for column in range(0,row,len(kata): # lopping column sesuai dengan jumlah row 
+            z+=list(kata[:row]) # tambah value list dengan index yang panjangya sesuai hasil looping row 
+        z+=list(kata[row]) #tambah value list denga index yang panjangnya sesuai dengan row
     gabung =''.join(z) #setelah itu ubah list z menjadi string dengan cara di gabung
     return gabung # kembalikan value yang ada di gabung
 print (urai(nama)) # cetak return pada fungsi urai
